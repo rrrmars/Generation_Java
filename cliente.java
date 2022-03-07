1699a9d2a59f765db26f05b8813e390fd0912bc1
Rosi <rosy.marttins@gmail.com>
2022-03-07 14:01:08 -0300

    um commit

---
+++ cliente.java
@@ -0,0 +1,23 @@
+package exercicios_POO;
+
+public class cliente {
+	
+	//atributos => variaveis
+     String primeiroNome;
+	 String ultimoNome;
+     int cpf;
+     int idade;
+     
+     
+     //metodos => atos
+     void pesquisar() {
+    	 System.out.println("O cliente esta pesquisando");
+     }
+     
+     void comprar() {
+    	 System.out.println("O cliente esta comprando");
+     }
+     
+	
+
+}
